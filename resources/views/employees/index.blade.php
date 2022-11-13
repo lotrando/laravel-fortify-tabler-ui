38a7ec89c45b665cf919d7f86308816b1910e6f1
Lotrando <miroslav.klika@seznam.cz>
2022-11-14 00:12:24 +0100

    chancge th css

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -35,15 +35,7 @@
                   <th>{{ __('Mobile') }}</th>
                   <th>{{ __('Status') }}</th>
                   <th>{{ __('Start date') }}</th>
-                  <th class="text-center">
-                    <svg class="text-azure" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 30" stroke-width="2" stroke="currentColor" fill="none"
-                      stroke-linecap="round" stroke-linejoin="round">
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                      <line x1="4" y1="6" x2="20" y2="6"></line>
-                      <line x1="4" y1="12" x2="20" y2="12"></line>
-                      <line x1="4" y1="18" x2="20" y2="18"></line>
-                    </svg>
-                  </th>
+                  <th class="text-center"><i class="fas fa-bars"></i></th>
                 </tr>
               </thead>
               <tbody>
