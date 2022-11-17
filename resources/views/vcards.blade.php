96955b6f6fb3813ed0de0c76421fb2e66354428e
Lotrando <miroslav.klika@seznam.cz>
2022-11-17 16:22:01 +0100

    fix css searchbox

--- resources/views/vcards.blade.php
+++ resources/views/vcards.blade.php
@@ -3,7 +3,7 @@
 @section('main')
   <div class="container-fluid mt-2">
     <div class="row mb-2">
-      <div class="col-3">
+      <div class="col-sm-12 col-lg-3">
         <div class="my-md-0 flex-grow-1 flex-md-grow-0 order-md-last card order-first p-1">
           <input class="form-control" id="search" type="text" value="" aria-label="Search in website" placeholder="{{ __('Search...') }}">
           <div id="display"></div>
