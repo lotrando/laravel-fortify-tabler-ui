fe12fc61c0e584f5d4127589b74a5be9f3e0094e
Lotrando <miroslav.klika@seznam.cz>
2022-11-17 16:30:03 +0100

    css fix

--- resources/views/vcards.blade.php
+++ resources/views/vcards.blade.php
@@ -4,7 +4,7 @@
   <div class="container-fluid mt-2">
     <div class="row mb-2">
       <div class="col-sm-12 col-lg-3">
-        <div class="my-md-0 flex-grow-1 flex-md-grow-0 order-md-last card order-first p-1">
+        <div class="my-md-0 flex-grow-1 flex-md-grow-0 order-md-last card order-first p-1 shadow-lg">
           <input class="form-control" id="search" type="text" value="" aria-label="Search in website" placeholder="{{ __('Search...') }}">
           <div id="display"></div>
         </div>
